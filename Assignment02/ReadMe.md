ba9482d7f09adb0ed5a3780141338022e16c563f
Sawyer Reed <98373457+sawyerreed14@users.noreply.github.com>
2023-07-17 12:54:59 -0700

    Add files via upload

---
+++ Assignment02/ReadMe.md
@@ -0,0 +1,3 @@
+# Week 2 Thoughts
+
+This weeks work was fairly simple and I like how it eases into JavaScript rather than jumping in head first with no prior knowledge. I had a lot of fun messing around on the JavaScript website and I think I will enjoy going more into depth with this! Please let me know if I need to fix anything with my console log or file structure.
---
+++ Assignment02/script.js
@@ -0,0 +1,14 @@
+/*
+Author: Sawyer Reed
+Date: 07/11/23
+Purpose: Assignment02
+*/
+
+// My top 3 websites.
+console.log("Youtube, Google, and Adobe.");
+
+// My 3 favorite games.
+console.log("Baseball, Basketball, and for video games, Rocket League.");
+
+// My 3 favorite artists and their cultural importance.
+console.log("I would go with the the Isley brothers as my three favorite artists because there are three of them in the group. They are important for the culture of music becuase they were original and paved the way for many other artists who have modeled their music after them or even sampled it. Some examples of songs that use the Isleys instrumentals are 'It Was A Good Day' by Ice Cube and 'Big Poppa' by Biggie.");
